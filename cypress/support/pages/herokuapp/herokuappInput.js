d95b49b037ee442f2a3b581255d0624955a7552a
Fabiana Veloso <fabianaveloso@gmail.com>
2023-02-18 22:42:11 -0300

    Dropdown code

--- cypress/support/pages/herokuapp/herokuappInput.js
+++ cypress/support/pages/herokuapp/herokuappInput.js
@@ -1,15 +1,18 @@
 class Input {
     fieldNumber = "[type='number']"
     
-
-
     visitPageInput(){
     cy.visit(Cypress.env("herokuapp")+'/inputs')
     }
 
-    increaseNumber(numberUser){
+    increaseNumber(numberUser,numvezes){
+        const contador1 = []
+        contador1.length = numvezes
         cy.get(this.fieldNumber).type(numberUser)
-        cy.get("[type='number']").type('{upArrow}{upArrow}{upArrow}')
+        cy.wrap(contador1).each(() => {
+            cy.get("[type='number']").type('{upArrow}')
+        })
+        cy.get(this.fieldNumber).should('have.value', parseInt(numberUser)+parseInt(numvezes))//have.value = valor do campo
         
     }
     decreaseNumber(numberUser){
@@ -20,4 +23,3 @@ class Input {
 
 }
 export default Input 
-
